c8a85ce633b02435a366f3991ddbbce91767d4ab
Aomi Jokoji <ajokoji@change.org>
2021-01-30 10:43:05 -0800

    setup socketio backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import * as http from "http";
 import examplesRouter from "./routes/examples";
 import path from "path";
+import { Socket } from "socket.io";
 
 dotenv.config();
 
@@ -15,11 +16,15 @@ const CLIENT_BUILD_RELATIVE_PATH = "../../client/build";
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use(
   "/static",
   express.static(path.join(__dirname, CLIENT_BUILD_RELATIVE_PATH + "/static"))
 );
+
+const server = new http.Server(app);
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const io = require("socket.io")(server);
 
 try {
   mongoose.connect(DB_URI, {
@@ -39,10 +44,41 @@ connection.once("open", () => {
 
 app.use("/api/examples", examplesRouter);
 
+io.on("connection", (socket: Socket) => {
+  console.log("Connected succesfully to the socket ...");
+
+  const news = [
+    {
+      title: "The cure of the Sadness is to play Videogames",
+      date: "04.10.2016",
+    },
+    {
+      title: "Batman saves Racoon City, the Joker is infected once again",
+      date: "05.10.2016",
+    },
+    {
+      title: "Deadpool doesn't want to do a third part of the franchise",
+      date: "05.10.2016",
+    },
+    {
+      title:
+        "Quicksilver demand Warner Bros. due to plagiarism with Speedy Gonzales",
+      date: "04.10.2016",
+    },
+  ];
+
+  // Send news on the socket
+  socket.emit("news", news);
+
+  socket.on("my other event", (data) => {
+    console.log(data);
+  });
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, CLIENT_BUILD_RELATIVE_PATH, "index.html"));
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
 });
