68fc72bd3484d9ff34f51032790645c3ef42354a
Shea Faigan <stfaigan@gmail.com>
2021-01-30 12:28:13 -0800

    add front-end data types

--- client/src/types.ts
+++ client/src/types.ts
@@ -7,3 +7,38 @@ export interface Example {
 
 export type ExampleAdd = Pick<Example, "foo" | "bar">;
 export type ExampleUpdate = Partial<Pick<Example, "foo" | "bar">>;
+
+export interface Position {
+  x: number;
+  y: number;
+}
+
+export interface Piece {
+  pieceType: string;
+  position: Position;
+  colour: string;
+}
+
+export interface Square {
+  piece: Piece;
+  colour: string;
+}
+export interface Board {
+  size: number;
+  squares: Square[][];
+}
+
+export enum PieceType {
+  PAWN = "pawn",
+  BISHOP = "bishop",
+  ROOK = "rook",
+  KNIGHT = "knight",
+  QUEEN = "queen",
+  KING = "king",
+  VANGUARD = "vanguard",
+}
+
+export enum Colour {
+  WHITE = "white",
+  BLACK = "black",
+}
