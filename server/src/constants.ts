95146c331ea0bc79b7046b833f4957b3ad169462
Keith <khr8771@gmail.com>
2021-01-30 16:43:30 -0800

    add functions to find valid moves for given piece

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -13,6 +13,98 @@ export enum Colour {
   BLACK = "black",
 }
 
+export const TEST = {
+  size: 8,
+  squares: [
+    [
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+    ],
+    [
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+    ],
+    [
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+    ],
+    [
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+    ],
+    [
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      {
+        colour: Colour.WHITE,
+        piece: {
+          pieceType: PieceType.KNIGHT,
+          colour: Colour.WHITE,
+        },
+      },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+    ],
+    [
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+    ],
+    [
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+    ],
+    [
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+      { colour: Colour.WHITE },
+      { colour: Colour.BLACK },
+    ],
+  ],
+};
+
 export const BOARD_8 = {
   size: 8,
   squares: [
--- server/src/utils/board.ts
+++ server/src/utils/board.ts
@@ -1,11 +1,70 @@
-import { Board } from "../models/board";
-import { BOARD_8 } from "../constants";
+/* eslint-disable import/namespace */
+import { Board, Piece, Square } from "../models/board";
+import { BOARD_8, Colour, PieceType, TEST } from "../constants";
+import { Position } from "../types";
+import { move } from "./moves";
 
 export const generateBoard = (size: number): Board => {
   switch (size) {
     case 8:
       return BOARD_8;
     default:
-      return BOARD_8;
+      return TEST;
   }
+};
+
+export const isFriendlyOccupied = (
+  colour: string,
+  pos: Position,
+  board: Board
+): boolean => {
+  const occupant = board.squares[pos.y][pos.x].piece;
+  if (occupant) {
+    if (occupant.colour === colour) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const isEnemyOccupied = (
+  colour: string,
+  pos: Position,
+  board: Board
+): boolean => {
+  const occupant = board.squares[pos.y][pos.x].piece;
+  if (occupant) {
+    if (occupant.colour !== colour) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const isCheck = (
+  colour: string,
+  pos: Position,
+  board: Board
+): boolean => {
+  for (let j = 0; j < board.size; j++) {
+    for (let i = 0; i < board.size; i++) {
+      const square = board.squares[j][i];
+      const piecePos = { x: i, y: j };
+      if (square.piece) {
+        if (
+          square.piece.pieceType.toLowerCase() === PieceType.KING ||
+          square.piece.pieceType ||
+          square.piece.colour === colour
+        ) {
+          continue;
+        }
+
+        const validMoves: Position[] = move(piecePos, square.piece, board);
+        if (validMoves.includes(pos)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 };
